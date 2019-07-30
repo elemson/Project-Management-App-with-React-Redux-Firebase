fc1149d9be7f5cac9a8e72ef16f4e06d6a6d485b
Elemson <toluwhani@gmail.com>
2019-07-30 19:56:40 +0300

    fleshing out the login and signup component/form

--- proplan/src/App.js
+++ proplan/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./component/layout/Navbar";
 import Dashboard from "./component/dashboard/Dashboard";
 import ProjectDetails from "./component/project/projectDetails";
+import SignIn from "./component/auth/SignIn";
+import SignUp from "./component/auth/SignUp";
+
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +14,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Dashboard} />
           <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
