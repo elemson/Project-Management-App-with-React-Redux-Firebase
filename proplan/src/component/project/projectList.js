518b21f7242afec0605c9322011e986975fe955b
Elemson <toluwhani@gmail.com>
2019-07-30 18:39:39 +0300

    fleshing out the Project Summary component

--- proplan/src/component/project/projectList.js
+++ proplan/src/component/project/projectList.js
@@ -1,31 +1,12 @@
 import React from "react";
+import ProjectSummary from "./projectSummary";
 
 const projectList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey text text-darken-2">
-          <span className="card-title">Project Titles</span>
-          <p>Posted by the Net Ninja</p>
-          <p className="grey-text"> 3rd September, 2am</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey text text-darken-2">
-          <span className="card-title">Project Titles</span>
-          <p>Posted by the Net Ninja</p>
-          <p className="grey-text"> 3rd September, 2am</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey text text-darken-2">
-          <span className="card-title">Project Titles</span>
-          <p>Posted by the Net Ninja</p>
-          <p className="grey-text"> 3rd September, 2am</p>
-        </div>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   );
 };
---
+++ proplan/src/component/project/projectSummary.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const projectSummary = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey text text-darken-2">
+        <span className="card-title">Project Titles</span>
+        <p>Posted by the Net Ninja</p>
+        <p className="grey-text"> 3rd September, 2am</p>
+      </div>
+    </div>
+  );
+};
+
+export default projectSummary;
