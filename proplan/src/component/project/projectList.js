ceb0608be1d9e5d95101e9c4f96b67eea960ab93
Elemson <toluwhani@gmail.com>
2019-07-30 18:31:25 +0300

    fleshing out the Dashboard component

---
+++ proplan/src/component/project/projectList.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const projectList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey text text-darken-2">
+          <span className="card-title">Project Titles</span>
+          <p>Posted by the Net Ninja</p>
+          <p className="grey-text"> 3rd September, 2am</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey text text-darken-2">
+          <span className="card-title">Project Titles</span>
+          <p>Posted by the Net Ninja</p>
+          <p className="grey-text"> 3rd September, 2am</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey text text-darken-2">
+          <span className="card-title">Project Titles</span>
+          <p>Posted by the Net Ninja</p>
+          <p className="grey-text"> 3rd September, 2am</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default projectList;
