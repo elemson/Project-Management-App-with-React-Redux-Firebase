a81eb7f8066368c9ae99454b2310ecbfd3bd74c6
elemson <toluwhani@gmail.com>
2019-10-19 03:03:11 +0300

    Tracked Auth Status

--- proplan/src/component/layout/Navbar.js
+++ proplan/src/component/layout/Navbar.js
@@ -4,15 +4,16 @@ import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
 import { connect } from "react-redux";
 
-const Navbar = () => {
+const Navbar = props => {
+  const { auth } = props;
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
           proPlan
         </Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        {links}
       </div>
     </nav>
   );
@@ -20,6 +21,8 @@ const Navbar = () => {
 
 const mapStateToProps = state => {
   console.log(state);
-  return {};
+  return {
+    auth: state.firebase.auth
+  };
 };
 export default connect(mapStateToProps)(Navbar);
